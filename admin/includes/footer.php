94e7e7e6ce9db21bcfe354fa1eecb74e36eb75c9
isaac bonsu <youro@DESKTOP-U9O95EJ.olacoe.edu.gh>
2020-01-18 12:49:40 +0000

    first

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>OLA E-Voting System</b>
+    </div>
+    <strong>Developed By Isaac Roger Kweku Bonsu @2019</strong>
+</footer>
---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>OLA E-Voting System</b>
+      </div>
+      <strong>Developed by Isaac Roger Kweku Bonsu @ 2019</strong>
+    </div>
+    <!-- /.container -->
+</footer>
